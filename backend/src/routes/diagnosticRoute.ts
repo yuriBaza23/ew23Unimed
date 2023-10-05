a7c525e3a52ac3ae870e860fbdcddb3429cef205
Felipe Medici <ic1dem.contato@gmail.com>
2023-10-05 08:25:10 -0300

    feat: diagnostcs route

--- backend/src/routes/diagnosticRoute.ts
+++ backend/src/routes/diagnosticRoute.ts
@@ -1,11 +1,13 @@
-import express, { Response } from 'express'
+import express, { Request, Response } from 'express'
 import { resolverController } from '../adapters/resolverController';
 import { DiagnosticRepository } from '../repositories/DiagnosticRepository';
+import { GetDiagnosticsController } from './controllers/GetDiagnosticsController';
 
 export const diagnosticsRoute = express.Router();
 const diagnosticsRepo = new DiagnosticRepository()
+const getDiagnostics = new GetDiagnosticsController(diagnosticsRepo);
 
-diagnosticsRoute.get('/', resolverController(async (_: Request, res: Response) => {
-    let all = await diagnosticsRepo.getAll();
-    return res.status(200).json(all) 
+
+diagnosticsRoute.get('/', resolverController(async (req: Request, res: Response) => {
+    return await getDiagnostics.handle(req, res);
 }))
